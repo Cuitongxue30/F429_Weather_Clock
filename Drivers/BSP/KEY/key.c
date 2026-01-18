d22e5ce853ead4043318a8bd39738a5cd81c3aaa
CuiZhongxu <1216220709@qq.com>
2026-01-18 17:52:03 +0800

    重构按键驱动，支持多种触发模式

--- Drivers/BSP/KEY/key.c
+++ Drivers/BSP/KEY/key.c
@@ -1,9 +1,10 @@
 #include "key.h"
 
 Button_t Buttonlist[] = {
-    {KEY0_GPIO_Port, KEY0_Pin, GPIO_PIN_RESET, KEY_STATE_IDLE, KEY0},
-    {KEY1_GPIO_Port, KEY1_Pin, GPIO_PIN_RESET, KEY_STATE_IDLE, KEY1},
-    {KEY2_GPIO_Port, KEY2_Pin, GPIO_PIN_RESET, KEY_STATE_IDLE, KEY2},
+    /*端口, 引脚, 有效电平, 状态, 计时器,  模式,  短按事件, 长按事件*/
+    {KEY0_GPIO_Port, KEY0_Pin, GPIO_PIN_RESET, KEY_STATE_IDLE, 0, MODE_TRIGGRR_UP, KEY_EVENT_UP, 0},
+    {KEY1_GPIO_Port, KEY1_Pin, GPIO_PIN_RESET, KEY_STATE_IDLE, 0, MODE_TRIGGRR_DOWN, KEY_EVENT_DOWN, 0},
+    {KEY2_GPIO_Port, KEY2_Pin, GPIO_PIN_RESET, KEY_STATE_IDLE, 0, MODE_LONG_PRESS, KEY_EVENT_CONFIRM, KEY_EVENT_CANCEL},
 };
 
 void Key_Init(void)
@@ -16,18 +17,32 @@ uint8_t Button_Process_Single(Button_t *button)
     uint8_t is_pressed = (current_level == button->active_level);
     uint8_t return_id = 0;
 
+    if (button->mode == MODE_TOGGLE_SWITCH)
+    {
+        // static last_state = 0;
+        // if (is_pressed && !last_state)
+        // {
+        // }
+        return 0;
+    }
+
     switch (button->state)
     {
     case KEY_STATE_IDLE:
         if (is_pressed)
         {
             button->state = KEY_STATE_DEBOUNCE;
+            button->time = 0;
         }
         break;
     case KEY_STATE_DEBOUNCE:
         if (is_pressed)
         {
             button->state = KEY_STATE_PRESSED;
+            if (button->mode == MODE_TRIGGRR_DOWN)
+            {
+                return_id = button->id_shortEvent;
+            }
         }
         else
         {
@@ -35,10 +50,40 @@ uint8_t Button_Process_Single(Button_t *button)
         }
         break;
     case KEY_STATE_PRESSED:
+        if (is_pressed)
+        {
+            if (button->mode == MODE_LONG_PRESS)
+            {
+                button->time++;
+                if (button->time >= PRESS_THRESHOLD)
+                {
+                    return_id = button->id_longEvent;
+                    button->state = KEY_STATE_WAIT_RELEASE;
+                }
+            }
+        }
+        else
+        {
+            if (button->mode == MODE_TRIGGRR_UP)
+            {
+                return_id = button->id_shortEvent;
+            }
+            else if (button->mode == MODE_LONG_PRESS)
+            {
+                if (button->time <= PRESS_THRESHOLD)
+                {
+                    return_id = button->id_shortEvent;
+                }
+
+                return_id = button->id_shortEvent;
+            }
+            button->state = KEY_STATE_IDLE;
+        }
+        break;
+    case KEY_STATE_WAIT_RELEASE:
         if (!is_pressed)
         {
             button->state = KEY_STATE_IDLE;
-            return_id = button->id;
         }
         break;
     }
--- Drivers/BSP/KEY/key.h
+++ Drivers/BSP/KEY/key.h
@@ -8,29 +8,45 @@
 
 typedef enum
 {
-    KEY_NONE = 0,
-    KEY0,
-    KEY1,
-    KEY2,
-} KeyNumEnum;
+    KEY_NONE_EVENT = 0,
+    KEY_EVENT_CONFIRM,
+    KEY_EVENT_CANCEL,
+    KEY_EVENT_UP,
+    KEY_EVENT_DOWN,
+    KEY_EVENT_ON,
+    KEY_EVENT_OFF,
+} KeyEvent_t;
 
 typedef enum
 {
-    KEY_STATE_IDLE = 0,
-    KEY_STATE_DEBOUNCE,
-    KEY_STATE_PRESSED,
+    MODE_TRIGGRR_UP = 0, // 释放触发
+    MODE_TRIGGRR_DOWN,   // 按下触发
+    MODE_LONG_PRESS,     // 长按触发
+    MODE_TOGGLE_SWITCH   // 切换开关模式
+} KeyMode_t;
+
+typedef enum
+{
+    KEY_STATE_IDLE = 0,     // 空闲状态
+    KEY_STATE_DEBOUNCE,     // 消抖状态
+    KEY_STATE_PRESSED,      // 按下状态
+    KEY_STATE_WAIT_RELEASE, // 等待释放状态
 } KeyState_t;
 
 typedef struct
 {
-    GPIO_TypeDef *port;
-    uint16_t pin;
-    uint8_t active_level;
-    KeyState_t state;
-    uint8_t id;
+    GPIO_TypeDef *port;    //* GPIO端口 */
+    uint16_t pin;          //* GPIO引脚 */
+    uint8_t active_level;  //* 有效电平 */
+    KeyState_t state;      //* 按键状态 */
+    uint32_t time;         //* 计时器 */
+    KeyMode_t mode;        //* 按键模式 */
+    uint8_t id_shortEvent; //* 短按事件ID */
+    uint8_t id_longEvent;  //* 长按事件ID */
 } Button_t;
 
 #define BUTTON_COUNT (sizeof(Buttonlist) / sizeof(Button_t))
+#define PRESS_THRESHOLD 50
 
 uint8_t Button_Process_Single(Button_t *button);
 uint8_t Key_Scan_All(void);
